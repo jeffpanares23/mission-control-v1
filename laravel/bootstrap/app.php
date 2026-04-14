2de1c874014cf058853701d36542a0c1e61c025e
Jeffy <panaresjeff23@gmail.com>
2026-04-14 08:37:09 +0000

    fix: add missing Laravel 11 skeleton files - artisan, public/index.php, config/app.php, routes/{console,web}.php, storage dirs, AppServiceProvider

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -10,12 +10,7 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->api(prepend: [
-            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
-        ]);
-        $middleware->alias([
-            'auth' => \App\Http\Middleware\Authenticate::class,
-        ]);
+        //
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
